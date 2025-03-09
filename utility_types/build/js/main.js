fb9c59247b35f3407a24c8ecb6d5ec4250278851
RYham <rafyahamza99@gmail.com>
2025-03-09 15:31:56 +0000

    In this section we learn what is the utility types in typescript

---
+++ utility_types/build/js/main.js
@@ -0,0 +1,71 @@
+"use strict";
+// Utility types
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
+};
+const assign1 = {
+    studentId: "compsci123",
+    title: "Final Project",
+    grade: 0,
+};
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded = updateAssignment(assign1, { grade: 95 });
+// Required and readonly
+const recordAssignment = (assign) => {
+    // send to database, etc.
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+// Record
+const hexColorMap = {
+    red: "FF0000",
+    green: "00FF00",
+    blue: "0000FF",
+};
+const finalGrades = {
+    Hamza: "B",
+    Dave: "C",
+};
+const gradeData = {
+    Hamza: { assign1: 23, assign2: 30 },
+    Dave: { assign1: 12, assign2: 21 },
+};
+const score = {
+    studentId: "k123",
+    grade: 85,
+};
+const preview = {
+    studentId: "k123",
+    title: "Final Project",
+};
+// ReturnType
+// type newAssign = { title: string; points: number };
+const createNewAssign = (id, title, points) => {
+    return { id, title, points };
+};
+const tsAssign = createNewAssign(1, "Utility types", 100);
+const assignArgs = [12, "Generics", 100];
+const tsAssign2 = createNewAssign(...assignArgs);
+// Awaited - help us with the ReturnType of a Promise
+const fetchUsers = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+        return res.json();
+    })
+        .catch((err) => {
+        if (err instanceof Error)
+            console.log(err.message);
+    });
+    return data;
+});
+fetchUsers().then((users) => console.log(users));
