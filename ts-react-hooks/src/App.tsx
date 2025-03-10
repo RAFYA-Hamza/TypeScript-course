851797f03efc5833589544e82c453a5cc0d57a07
RYham <rafyahamza99@gmail.com>
2025-03-10 17:51:44 +0000

    In this section we have discover the basics hooks with typescript

--- ts-react-hooks/src/App.tsx
+++ ts-react-hooks/src/App.tsx
@@ -1,8 +1,62 @@
+import {
+  KeyboardEvent,
+  MouseEvent,
+  useCallback,
+  useMemo,
+  useRef,
+  useEffect,
+  useState,
+} from "react";
+
+interface User {
+  id: number;
+  username: string;
+}
+type fibFunc = (n: number) => number;
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+};
+
+const myNum: number = 37;
+
 function App() {
+  const [count, setCount] = useState<number>(0);
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  console.log(inputRef?.current);
+  console.log(inputRef?.current?.value);
+
+  useEffect(() => {
+    console.log("mounting");
+    console.log("Users: ", users);
+
+    return () => console.log("unmouting");
+  }, [users]);
+
+  const addTwo = useCallback(
+    (
+      e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
+    ): void => {
+      console.log("useCallback");
+
+      setCount((prev) => prev + 1);
+    },
+    []
+  );
+
+  const result = useMemo<number>(() => fib(myNum), [myNum]);
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <p>{result}</p>
+      <input ref={inputRef} type="text" />
+    </div>
   );
 }
 
